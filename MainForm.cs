87c1c2b9b4e9cbcc1f6a64e12046b916a54d6dde
Andrew-Eathan <42521608+Andrew-Eathan@users.noreply.github.com>
2022-10-30 19:55:01 +0200

    le fixe

--- MainForm.cs
+++ MainForm.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using System.Media;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace AutoSaver
 {
@@ -78,17 +79,10 @@ namespace AutoSaver
                     test.Tag = process;
                     test.ImageIndex = AvailableProcesses.SmallImageList.Images.Count;
 
-                    string fileName;
-                    try // for whatever reason, trying to access MainModule of some processes throws an access denied win32 exception
-                    {
-                        fileName = process.MainModule?.FileName ?? "nonelol";
-                        Icon? pick = fileName == "nonelol" ? ASResources.program : Icon.ExtractAssociatedIcon(fileName);
+                    Icon? pick = process.GetIcon() ?? ASResources.program;
 
-                        AvailableProcesses.SmallImageList.Images.Add(pick);
-                        SelectedProcesses.SmallImageList.Images.Add(pick);
-                    }
-                    catch {}
-
+                    AvailableProcesses.SmallImageList.Images.Add(pick);
+                    SelectedProcesses.SmallImageList.Images.Add(pick);
                     AvailableProcesses.Items.Add(test);
                 }
             }
@@ -285,5 +279,31 @@ public static class ProcessExtensions
             return false;
         }
         return true;
+    }
+
+    [DllImport("Kernel32.dll")]
+    private static extern uint QueryFullProcessImageName([In] IntPtr hProcess, [In] uint dwFlags, [Out] StringBuilder lpExeName, [In, Out] ref uint lpdwSize);
+
+    public static string? GetMainModuleFileName(this Process process, int buffer = 1024)
+    {
+        var fileNameBuilder = new StringBuilder(buffer);
+        uint bufferLength = (uint)fileNameBuilder.Capacity + 1;
+        return QueryFullProcessImageName(process.Handle, 0, fileNameBuilder, ref bufferLength) != 0 ?
+            fileNameBuilder.ToString() :
+            null;
+    }
+
+    public static Icon? GetIcon(this Process process)
+    {
+        try
+        {
+            string mainModuleFileName = process.GetMainModuleFileName() ?? "cmd.exe";
+            return Icon.ExtractAssociatedIcon(mainModuleFileName);
+        }
+        catch
+        {
+            // Probably no access
+            return null;
+        }
     }
 }
